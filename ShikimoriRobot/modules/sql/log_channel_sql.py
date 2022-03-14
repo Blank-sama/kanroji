fc06511e6748063a91ce507b4b18f88b8340d0b1
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 01:18:18 +0530

    Update log_channel_sql.py

--- ShikimoriRobot/modules/sql/log_channel_sql.py
+++ ShikimoriRobot/modules/sql/log_channel_sql.py
@@ -28,7 +28,7 @@ import threading
 
 from sqlalchemy import Column, String, distinct, func
 
-from ErinaRobot.modules.sql import BASE, SESSION
+from ShikimoriRobot.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
