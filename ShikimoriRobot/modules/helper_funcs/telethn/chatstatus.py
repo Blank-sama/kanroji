7b04a7433f9f0ec0623af6375b53aaaecc4bee63
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 00:54:23 +0530

    Shikimori

--- ShikimoriRobot/modules/helper_funcs/telethn/chatstatus.py
+++ ShikimoriRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from ErinaRobot import REDLIONS
-from ErinaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from ShikimoriRobot import REDLIONS
+from ShikimoriRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
 
 
 async def user_is_ban_protected(user_id: int, message):
