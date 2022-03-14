993f169dd63a1f863a27a13dde29c032d1a5c8c4
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 00:55:52 +0530

    Shikimori

--- ShikimoriRobot/modules/helper_funcs/extraction.py
+++ ShikimoriRobot/modules/helper_funcs/extraction.py
@@ -29,8 +29,8 @@ from typing import List, Optional
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from ErinaRobot import LOGGER
-from ErinaRobot.modules.users import get_user_id
+from ShikimoriRobot import LOGGER
+from ShikimoriRobot.modules.users import get_user_id
 
 
 def id_from_reply(message):
