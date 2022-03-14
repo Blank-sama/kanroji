5a2155d6bcebf3d139dd9cb9ecf3347b36a1220f
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 00:58:35 +0530

    Shikimori

--- ShikimoriRobot/modules/helper_funcs/msg_types.py
+++ ShikimoriRobot/modules/helper_funcs/msg_types.py
@@ -28,7 +28,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from ErinaRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from ShikimoriRobot.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
