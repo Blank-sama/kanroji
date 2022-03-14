f8e657695449f23350f1d58224c0685eeb942589
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 01:06:13 +0530

    Update chatbot_sql.py

--- ShikimoriRobot/modules/sql/chatbot_sql.py
+++ ShikimoriRobot/modules/sql/chatbot_sql.py
@@ -28,7 +28,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from ErinaRobot.modules.sql import BASE, SESSION
+from ShikimoriRobot.modules.sql import BASE, SESSION
 
 
 class KukiChats(BASE):
