2c3de2f72ba54f7cf43003520ba5fa6d83b32850
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 01:05:50 +0530

    Update blacklistusers_sql.py

--- ShikimoriRobot/modules/sql/blacklistusers_sql.py
+++ ShikimoriRobot/modules/sql/blacklistusers_sql.py
@@ -28,7 +28,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from ErinaRobot.modules.sql import BASE, SESSION
+from ShikimoriRobot.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
