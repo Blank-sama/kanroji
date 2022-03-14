a6c371c555d6bb4e72b434f5c29dbe75e0879670
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 00:46:30 +0530

    Shikimori

--- ShikimoriRobot/utils/permission.py
+++ ShikimoriRobot/utils/permission.py
@@ -3,9 +3,9 @@ from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 
-from ErinaRobot import REDLIONS
-from ErinaRobot import pbot as app
-from ErinaRobot.pyrogramee.pluginshelper import member_permissions
+from ShikimoriRobot import REDLIONS
+from ShikimoriRobot import pbot as app
+from ShikimoriRobot.pyrogramee.pluginshelper import member_permissions
 
 
 async def authorised(func, subFunc2, client, message, *args, **kwargs):
