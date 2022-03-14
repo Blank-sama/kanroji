f413bfae7bbd555f411d682109f4ed0f2a36fa48
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 00:52:12 +0530

    Shikimori

--- ShikimoriRobot/programee/pluginshelper.py
+++ ShikimoriRobot/programee/pluginshelper.py
@@ -9,7 +9,7 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from ErinaRobot import OWNER_ID, pbot
+from ShikimoriRobot import OWNER_ID, pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
