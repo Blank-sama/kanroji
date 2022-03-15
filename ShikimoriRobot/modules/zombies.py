8009fda35b75d54ff41b484d2be5fdc4ff83f5dd
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 12:12:16 +0530

    Update zombies.py

--- ShikimoriRobot/modules/zombies.py
+++ ShikimoriRobot/modules/zombies.py
@@ -5,7 +5,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChannelParticipantsAdmins, ChatBannedRights
 
-from ErinaRobot import DEV_USERS, OWNER_ID, REDLIONS, SPRYZONS, telethn
+from ShikimoriRobot import DEV_USERS, OWNER_ID, REDLIONS, SPRYZONS, telethn
 
 # =================== CONSTANT ===================
 
