62cb26e0a356f3265cc5d82f45617420c122d33d
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 23:21:25 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop ErinaRobot
-net start ErinaRobot
+net stop ShikimoriRobot
+net start ShikimoriRobot
