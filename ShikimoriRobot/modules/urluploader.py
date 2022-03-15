5fa03b3374de3d2bd9cb88135ff0c43417b2211d
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 12:22:09 +0530

    Update urluploader.py

--- ShikimoriRobot/modules/urluploader.py
+++ ShikimoriRobot/modules/urluploader.py
@@ -3,7 +3,7 @@ import time
 
 import aiohttp
 
-from ErinaRobot.pyrogramee.pluginshelper import humanbytes, time_formatter
+from ShikimoriRobot.pyrogramee.pluginshelper import humanbytes, time_formatter
 
 
 async def download_file(url, file_name, message, start_time, bot):
