ff3655bf7afb1fa53fe0913723e2e32326ecc029
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-15 13:00:28 +0530

    Update books.py

--- ShikimoriRobot/modules/books.py
+++ ShikimoriRobot/modules/books.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from telethon import events
 
-from ErinaRobot import telethn as tbot
+from ShikimoriRobot import telethn as tbot
 
 
 @tbot.on(events.NewMessage(pattern="^/book (.*)"))
