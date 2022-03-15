791992d8c9e6896b37d332a2bc2d41a36a27a58e
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-16 00:06:30 +0530

    Update stickers.py

--- ShikimoriRobot/modules/stickers.py
+++ ShikimoriRobot/modules/stickers.py
@@ -11,8 +11,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from ErinaRobot import dispatcher
-from ErinaRobot.modules.disable import DisableAbleCommandHandler
+from ShikimoriRobot import dispatcher
+from ShikimoriRobot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
