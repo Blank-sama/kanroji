10358dcd4622df509cbabdbf183af995fcb701ca
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-16 09:18:48 +0530

    Update alive.py

--- ShikimoriRobot/modules/alive.py
+++ ShikimoriRobot/modules/alive.py
@@ -19,13 +19,11 @@ from pyrogram import __version__ as pyro
 edit_time = 5
 """ =======================CONSTANTS====================== """
 file1 = "https://telegra.ph//file/38d71dde31b5f10b04d77.jpg"
-file2 = "https://telegra.ph//file/9b2f4c22aeecbf77d26e4.jpg"
-file3 = "https://telegra.ph//file/ad7c7ccdd4db3795f0d50.jpg"
-file4 = "https://telegra.ph//file/2643f9768a424a0a61991.jpg" 
-file5 = "https://telegra.ph//file/62a5a4b9bf563caac526b.jpg"
-file6 = "https://telegra.ph//file/bead833fe37b0dc1f42d4.jpg"
-file7 = "https://telegra.ph//file/ac577854aeb7b21d6bbc7.jpg"
-file8 = "https://telegra.ph//file/b0e0068dc409ae71171d2.jpg"
+file2 = "https://telegra.ph//file/ad7c7ccdd4db3795f0d50.jpg"
+file3 = "https://telegra.ph//file/62a5a4b9bf563caac526b.jpg"
+file4 = "https://telegra.ph//file/bead833fe37b0dc1f42d4.jpg"
+file5 = "https://telegra.ph//file/ac577854aeb7b21d6bbc7.jpg"
+file6 = "https://telegra.ph//file/b0e0068dc409ae71171d2.jpg"
 """ =======================CONSTANTS====================== """
 
 START_TIME = datetime.utcnow()
@@ -55,38 +53,30 @@ async def hmm(yes):
     current_time = datetime.utcnow()
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
-    Shu = f"♡ **Hey [{yes.sender.first_name}](tg://user?id={yes.sender.id}), I'm Shikimori**\n\n"
-    Shu += f"♡ **My Uptime** ~♪ `{uptime}`\n\n"
-    Shu += f"♡ **Telethon Version** ~♪ `{version.__version__}`\n\n"
-    Shu += f"♡ **Python Telegram Bot Version** ~♪ `{telegram.__version__}`\n\n"
-    Shu += f"♡ **Pyrogram Version** ~♪ `{pyro}`\n\n"
+    Shu = f"♡ **Hey [{yes.sender.first_name}](tg://user?id={yes.sender.id}), I'm Shikimori**\n"
+    Shu += f"♡ **My Uptime** ~♪ `{uptime}`\n"
+    Shu += f"♡ **Telethon Version** ~♪ `{version.__version__}`\n"
+    Shu += f"♡ **Python Telegram Bot Version** ~♪ `{telegram.__version__}`\n"
+    Shu += f"♡ **Pyrogram Version** ~♪ `{pyro}`\n"
     Shu += f"♡ **My Darling** ~♪ [Sawada Tsunayoshi](tg://user?id={OWNER_ID})"
-    BUTTON = [[Button.url("ʜᴇʟᴘ", f"https://t.me/Shikimori_Robot?start=help"),Button.url("sᴜᴘᴘᴏʀᴛ", f"https://t.me/KyoukoXsupport"),],[Button.url("♡ᴅᴀʀʟɪɴɢ♡", f"https://t.me/Sawada_Kun")]]
+    BUTTON = [[Button.url("ʜᴇʟᴘ", f"https://t.me/Shikimori_Robot?start=help"),Button.url("sᴜᴘᴘᴏʀᴛ", f"https://t.me/NobaraSupport"),],[Button.url("♡ᴅᴀʀʟɪɴɢ♡", f"https://t.me/Sawada_Kun")]]
     on = await borg.send_file(yes.chat_id, file=file2,caption=Shu, buttons=BUTTON)
 
     await asyncio.sleep(edit_time)
-    ok = await borg.edit_message(yes.chat_id, on, file=file8, buttons=BUTTON) 
+    ok = await borg.edit_message(yes.chat_id, on, file=file6, buttons=BUTTON) 
 
     await asyncio.sleep(edit_time)
-    ok2 = await borg.edit_message(yes.chat_id, ok, file=file7, buttons=BUTTON)
+    ok2 = await borg.edit_message(yes.chat_id, ok, file=file5, buttons=BUTTON)
 
     await asyncio.sleep(edit_time)
-    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file6, buttons=BUTTON)
+    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file4, buttons=BUTTON)
     
     await asyncio.sleep(edit_time)
-    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file5, buttons=BUTTON)
+    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3, buttons=BUTTON)
     
     await asyncio.sleep(edit_time)
-    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file4, buttons=BUTTON)
+    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2, buttons=BUTTON)
     
     await asyncio.sleep(edit_time)
-    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file3, buttons=BUTTON)
+    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1, buttons=BUTTON)
     
-    await asyncio.sleep(edit_time)
-    ok7 = await borg.edit_message(yes.chat_id, ok6, file=file2, buttons=BUTTON)
-
-    await asyncio.sleep(edit_time)
-    ok8 = await borg.edit_message(yes.chat_id, ok7, file=file1, buttons=BUTTON)
-
-    await asyncio.sleep(edit_time)
-    ok9 = await borg.edit_message(yes.chat_id, ok8, file=file8, buttons=BUTTON)
