2ee9069c35b89677d2d3e1d30f0f8bf9f5693ca9
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-29 02:13:20 +0530

    Update shippering.py

--- ShikimoriRobot/modules/shippering.py
+++ ShikimoriRobot/modules/shippering.py
@@ -4,8 +4,11 @@ from datetime import datetime
 from pyrogram import filters
 
 from ShikimoriRobot import pbot as app
-from ShikimoriRobot.modules.mongo.couples_mongo import get_couple, save_couple
 from ShikimoriRobot.utils.errors import capture_err
+from ShikimoriRobot.utils.dbfunctions import get_couple, save_couple
+
+__mod_name__ = "Shippering"
+__help__ = "/couples - To Choose Couple Of The Day"
 
 
 # Date and time
@@ -35,8 +38,7 @@ tomorrow = str(dt_tom())
 @capture_err
 async def couple(_, message):
     if message.chat.type == "private":
-        await message.reply_text("This command only works in groups.")
-        return
+        return await message.reply_text("This command only works in groups.")
     try:
         chat_id = message.chat.id
         is_selected = await get_couple(chat_id, today)
@@ -46,8 +48,7 @@ async def couple(_, message):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
-                await message.reply_text("Not enough users")
-                return
+                return await message.reply_text("Not enough users")
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
@@ -55,10 +56,12 @@ async def couple(_, message):
             c1_mention = (await app.get_users(c1_id)).mention
             c2_mention = (await app.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**Couple of the day:**
+            couple_selection_message = f"""**Nigga and Niggi of the day:**
 {c1_mention} + {c2_mention} = ❤️
-__New Nibba Nibbi of the day may be chosen at 12AM {tomorrow}__"""
-            await app.send_message(message.chat.id, text=couple_selection_message)
+__New Nigga And Niggi of the day will be chosen at 12AM {tomorrow}__"""
+            await app.send_message(
+                message.chat.id, text=couple_selection_message
+            )
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
 
@@ -69,11 +72,10 @@ __New Nibba Nibbi of the day may be chosen at 12AM {tomorrow}__"""
             c2_name = (await app.get_users(c2_id)).first_name
             couple_selection_message = f"""Couple of the day:
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
-__New Nibba Nibbi of the day may be chosen at 12AM {tomorrow}__"""
-            await app.send_message(message.chat.id, text=couple_selection_message)
+__New Nigga And Niggi of the day will be chosen at 12AM {tomorrow}__"""
+            await app.send_message(
+                message.chat.id, text=couple_selection_message
+            )
     except Exception as e:
         print(e)
-        message.reply_text(e)
-
-
-__mod_name__ = "Couples"
+        await message.reply_text(e)
