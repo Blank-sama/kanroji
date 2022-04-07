10d579c39e823c52eff96383750fa1a7ecf3712b
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-07 16:08:32 +0530

    Update decorators.py

--- ShikimoriRobot/modules/helper_funcs/decorators.py
+++ ShikimoriRobot/modules/helper_funcs/decorators.py
@@ -86,9 +86,7 @@ class ShikimoriTelegramHandler:
             self._dispatcher.add_handler(
                 InlineQueryHandler(pattern=pattern, callback=func, run_async=run_async, pass_user_data=pass_user_data,
                                    pass_chat_data=pass_chat_data, chat_types=chat_types))
-            LOGGER.debug(
-                f'[SHIKINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES '
-                f'USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}')
+            LOGGER.debug(f'[MIKUINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}')
             return func
 
         return _inlinequery
