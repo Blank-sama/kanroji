1c9e18e58ff819430205e6b4f337201a157f090c
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-07 16:23:01 +0530

    Update inlinebot.py

--- ShikimoriRobot/modules/inlinebot.py
+++ ShikimoriRobot/modules/inlinebot.py
@@ -24,7 +24,7 @@ from ShikimoriRobot import (
     sw, LOGGER
 )
 from ShikimoriRobot.modules.helper_funcs.misc import article
-from ShikimoriRobot.modules.helper_funcs.decorators import shikinline
+from ShikimoriRobot.modules.helper_funcs.decorators import shikimoriinline
 
 
 def remove_prefix(text, prefix):
@@ -32,7 +32,7 @@ def remove_prefix(text, prefix):
         text = text.replace(prefix, "", 1)
     return text
 
-@shikinline()
+@shikimoriinline()
 def inlinequery(update: Update, _) -> None:
     """
     Main InlineQueryHandler callback.
