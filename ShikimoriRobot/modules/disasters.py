9f17b83850df0fea7a054a00219ed2e47f81b89f
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-04-28 23:14:53 +0530

    Update disasters.py

--- ShikimoriRobot/modules/disasters.py
+++ ShikimoriRobot/modules/disasters.py
@@ -7,7 +7,7 @@ from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
 
-from YamatoRobot import (
+from ShikimoriRobot import (
     DEV_USERS,
     FAFNIRS,
     LUINORS,
@@ -16,13 +16,13 @@ from YamatoRobot import (
     SPRYZONS,
     dispatcher,
 )
-from YamatoRobot.modules.helper_funcs.chat_status import (
+from ShikimoriRobot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from YamatoRobot.modules.helper_funcs.extraction import extract_user
-from YamatoRobot.modules.log_channel import gloggable
+from ShikimoriRobot.modules.helper_funcs.extraction import extract_user
+from ShikimoriRobot.modules.log_channel import gloggable
 
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "YamatoRobot/elevated_users.json")
 
